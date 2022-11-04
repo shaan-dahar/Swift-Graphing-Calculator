a8a3c91b249ff93fd33a36eef3a51838a9f9ea2e
shaan-dahar <shaan.dahar@gmail.com>
2022-11-04 12:05:01 -0400

    Initial Commit

---
+++ GraphFunctCalc/GraphFunctCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphFunctCalcApp.swift
+//  GraphFunctCalc
+//
+//  Created by Student on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GraphFunctCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GraphFunctCalcUITests/GraphFunctCalcUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GraphFunctCalcUITestsLaunchTests.swift
+//  GraphFunctCalcUITests
+//
+//  Created by Student on 11/4/22.
+//
+
+import XCTest
+
+final class GraphFunctCalcUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
